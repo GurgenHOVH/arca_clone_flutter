8b7853b6f9a3938106763dfdeea32f6746cd164b
Gurgen Hovhannisyan <g.hovhannisyan@digitalpomegranate.com>
2020-08-31 12:44:22 +0400

    Initial commit

---
+++ lib/pages/cards_page.dart
@@ -0,0 +1,91 @@
+import 'package:arca/models/card_details.dart';
+import 'package:arca/widgets/card.dart';
+import 'package:flutter/material.dart';
+
+class CardsPage extends StatefulWidget {
+  @override
+  _CardsPageState createState() => _CardsPageState();
+}
+
+class _CardsPageState extends State<CardsPage> {
+  List<CardDetails> cards = [
+    CardDetails(
+      'AMERIABANK',
+      CardType.Master,
+      1430345698762345,
+      345900,
+      DateTime.now().subtract(Duration(hours: 1)),
+      DateTime(2022, 08, 01),
+    ),
+    CardDetails(
+      'AMERIABANK',
+      CardType.Visa,
+      1430345698762345,
+      345900,
+      DateTime.now().subtract(Duration(hours: 1)),
+      DateTime(2022, 08, 01),
+    ),
+  ];
+
+  int selectedCard = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xff1573CB),
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: SingleChildScrollView(
+          child: Column(children: [
+        Container(
+          height: 230,
+          width: MediaQuery.of(context).size.width,
+          child: PageView(
+            allowImplicitScrolling: true,
+            scrollDirection: Axis.horizontal,
+            onPageChanged: (index) {
+              setState(() {
+                selectedCard = index;
+              });
+            },
+            children: List.generate(cards.length, (index) {
+              return Center(
+                child: Padding(
+                  padding: EdgeInsets.all(selectedCard == index ? 0 : 25),
+                  child: CreditCard(cardDetails: cards[index]),
+                ),
+              );
+            }),
+          ),
+        ),
+        Column(
+          children: [
+            ListTile(
+              title: Text('wdwdwddw')
+            ),
+            ListTile(
+              title: Text('wdwdwddw')
+            ),
+            ListTile(
+              title: Text('wdwdwddw')
+            ),
+            ListTile(
+              title: Text('wdwdwddw')
+            ),
+            ListTile(
+              title: Text('wdwdwddw')
+            ),
+            ListTile(
+              title: Text('wdwdwddw')
+            ),
+            ListTile(
+              title: Text('wdwdwddw')
+            ),
+          ],
+        )
+      ])),
+    );
+  }
+}
